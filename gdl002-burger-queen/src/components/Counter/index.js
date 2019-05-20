2df57c2da7ae61ca2ea3346f7fa4cbbf9146d8fe
ClauGarfiasMx <apps@claudiagarfias.works>
2019-05-20 15:34:50 -0500

    Handling increment, decrement & reset, not working in every element

--- gdl002-burger-queen/src/components/Counter/index.js
+++ gdl002-burger-queen/src/components/Counter/index.js
@@ -2,53 +2,28 @@ import React, { Component } from "react";
 import Button from "../Button";
 
 class Counter extends Component {
-  state = {
-    item: this.props.name,
-    count: this.props.value,
-    price: this.props.price
-  };
-  //   constructor() {
-  //     super();
-  //     this.handleIncrement = this.handleIncrement.bind(this);
-  //   }
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  handleDecrement = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-  reset = () => {
-    this.setState({ count: 0 });
-  };
-  formatCount() {
-    const { count } = this.state;
-    if (count < 0) {
-      this.reset();
-    }
-    return count === 0 ? <label>Cant.</label> : <label>{count}</label>;
-  }
   render() {
     return (
       <div>
-        <label>{this.state.item}</label>
+        <label>{this.props.name}</label>
+        <label>{this.props.price}</label>
         <Button
           name="+"
-          action={this.handleIncrement}
+          action={this.props.handleIncrement}
           extraClassName="counter-btn"
         />
         <Button
           name="-"
-          action={this.handleDecrement}
-          extraClassName="couunter-btn"
-        />
-        <span>{this.formatCount()}</span>
-        <Button
-          name="borrar"
-          action={this.reset}
+          action={this.props.handleDecrement}
           extraClassName="counter-btn"
         />
-        <label>{this.state.price}</label>
+        <span>{this.props.value}</span>
+        <Button
+          name="borrar"
+          action={this.props.reset}
+          extraClassName="counter-btn"
+        />
+        <label>{this.props.value * this.props.price}</label>
       </div>
     );
   }
--- gdl002-burger-queen/src/components/MenuBreakfast/index.js
+++ gdl002-burger-queen/src/components/MenuBreakfast/index.js
@@ -4,25 +4,71 @@ import Counter from "../Counter";
 class MenuBreakfast extends Component {
   state = {
     menu: [
-      { id: "b01", name: "Café Americano", value: 0, price: 5 },
-      { id: "b02", name: "Café con Leche", value: 0, price: 7 },
-      { id: "b03", name: "Jugo de Frutas Natural", value: 0, price: 10 },
-      { id: "b04", name: "Sandwich de Jamón y Queso", value: 0, price: 10 }
+      { id: "b01", data: { name: "Café Americano", value: 0, price: 5 } },
+      { id: "b02", data: { name: "Café con Leche", value: 0, price: 7 } },
+      {
+        id: "b03",
+        data: { name: "Jugo de Frutas Natural", value: 0, price: 10 }
+      },
+      {
+        id: "b04",
+        data: { name: "Sandwich de Jamón y Queso", value: 0, price: 10 }
+      }
     ]
+    // subtotal: [(this.menu.subtotal = this.menu.value * this.menu.price)]
   };
+
+  handleIncrement = () => {
+    const nextState = this.state;
+    nextState.menu[0].data.value++;
+    this.setState(nextState);
+  };
+  handleDecrement = () => {
+    console.log("handleDecrement parent");
+    const nextState = this.state;
+    nextState.menu[0].data.value--;
+    this.setState(nextState);
+  };
+  reset = () => {
+    console.log("reset parent");
+    const nextState = this.state;
+    nextState.menu.data.value = 0;
+    this.setState(nextState);
+  };
+
   render() {
     return (
-      <section>
-        <h2>Menú Desayuno</h2>
-        {this.state.menu.map(counter => (
-          <Counter
-            key={counter.id}
-            name={counter.name}
-            value={counter.value}
-            price={`$${counter.price}`}
-          />
-        ))}
-      </section>
+      <main>
+        <section>
+          <h2>Menú Desayuno</h2>
+          {this.state.menu.map(counter => (
+            <Counter
+              key={counter.id}
+              id={counter.id}
+              name={counter.data.name}
+              value={counter.data.value}
+              price={counter.data.price}
+              handleIncrement={this.handleIncrement}
+              handleDecrement={this.handleDecrement}
+              reset={this.reset}
+            />
+          ))}
+          {console.log(this.state)}
+        </section>
+        {/* <section>
+          <h2>Orden</h2>
+          {this.state.menu
+            .filter(value => this.state.menu[0].value > 0)
+            .map(order => {
+              return (
+                <p>
+                  {order.name}
+                  {order.value}
+                </p>
+              );
+            })}
+        </section> */}
+      </main>
     );
   }
 }
