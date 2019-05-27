29c9e63371d1f87cead4a361e2a35f3e66fa1237
ClauGarfiasMx <apps@claudiagarfias.works>
2019-05-27 18:02:29 -0500

    Trying hashrouter

--- gdl002-burger-queen/src/components/App/index.js
+++ gdl002-burger-queen/src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Navigation from "../Navigation";
 import KitchenPage from "../Kitchen";
 import HomePage from "../Home";
