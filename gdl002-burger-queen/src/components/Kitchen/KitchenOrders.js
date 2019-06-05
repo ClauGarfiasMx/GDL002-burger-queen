53ca18ea76793be0eca3e8c74adf09c346fb253c
ClauGarfiasMx <apps@claudiagarfias.works>
2019-06-04 23:13:07 -0500

    Bug in last item, it does not disappear from list

--- gdl002-burger-queen/src/components/Kitchen/KitchenOrders.js
+++ gdl002-burger-queen/src/components/Kitchen/KitchenOrders.js
@@ -6,49 +6,47 @@ class Orders extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ordersArr: [],
-      cities: [],
+      newOrders: [],
+      prepOrders: [],
       db: Firebase.firestore()
     };
-    // this.getNewOrders = this.getNewOrders.bind(this);
   }
-  // getCollection() {
-  //   this.state.db
-  //     .collection("orders")
-  //     .where("status", "==", "new")
-  //     .get()
-  //     .then(onSnapshot => {
-  //       const ordersArr = [];
-  //       onSnapshot.forEach(doc => {
-  //         const incomingOrders = { orderID: doc.id, orderDetail: doc.data() };
-  //         ordersArr.push(incomingOrders);
-  //         this.setState({ ordersArr });
-  //         console.log(this.state.ordersArr);
-  //       });
-  //     });
-  // }
+
+  getOrders(status, arr) {
+    this.state.db
+      .collection("orders")
+      .where("status", "==", `${status}`)
+      .onSnapshot(querySnapshot => {
+        var newOrders = [];
+        querySnapshot.forEach(doc => {
+          const incomingOrders = { orderID: doc.id, orderDetail: doc.data() };
+          newOrders.push(incomingOrders);
+          this.setState({ arr });
+          console.log(this.state.arr);
+        });
+      });
+  }
 
   getNewOrders() {
     this.state.db
       .collection("orders")
       .where("status", "==", "new")
       .onSnapshot(querySnapshot => {
-        var ordersArr = [];
+        var newOrders = [];
         querySnapshot.forEach(doc => {
           const incomingOrders = { orderID: doc.id, orderDetail: doc.data() };
-          ordersArr.push(incomingOrders);
-          this.setState({ ordersArr });
-          console.log(this.state.ordersArr);
+          newOrders.push(incomingOrders);
+          this.setState({ newOrders });
         });
       });
   }
 
-  changeToPreparing = id => {
+  changeStatus = (id, status) => {
     this.state.db
       .collection("orders")
       .doc(id)
       .update({
-        status: "prep"
+        status: `${status}`
       })
       .then(function() {
         console.log("Document successfully written!");
@@ -57,53 +55,124 @@ class Orders extends Component {
         console.error("Error writing document: ", error);
       });
   };
+  getPrepOrders() {
+    this.state.db
+      .collection("orders")
+      .where("status", "==", "prep")
+      .onSnapshot(querySnapshot => {
+        var prepOrders = [];
+        querySnapshot.forEach(doc => {
+          const incomingOrders = { orderID: doc.id, orderDetail: doc.data() };
+          prepOrders.push(incomingOrders);
+          this.setState({ prepOrders });
+        });
+      });
+  }
 
+  // getOrders(status, arr) {
+  //   this.state.db
+  //     .collection("orders")
+  //     .where("status", "==", "new")
+  //     .onSnapshot(querySnapshot => {
+  //       var newOrders = [];
+  //       querySnapshot.forEach(doc => {
+  //         const incomingOrders = { orderID: doc.id, orderDetail: doc.data() };
+  //         newOrders.push(incomingOrders);
+  //         this.setState({ arr });
+  //         console.log(this.state.arr);
+  //       });
+  //     });
+  // }
   componentDidMount() {
     this.getNewOrders();
+    this.getPrepOrders();
+    // this.getOrders("new", "newOrders");
+    // this.getOrders("prep", "prepOrders");
   }
 
   render() {
     return (
-      <section>
-        <Button
-          name="Console log Collection"
-          action={() => {
-            this.getNewOrders();
-          }}
-        />
-        <h3> Pedidos </h3>
-        <div className="kitchen-orders">
-          {this.state.ordersArr.map((element, i) => {
-            return (
-              <div id={element.orderID} key={i} className="item-kitchen">
-                <span className="underline">
-                  Mesa: {element.orderDetail.table}
-                </span>
-                {element.orderDetail.items.map((it, i) => {
-                  return (
-                    <div key={i}>
-                      <p>
-                        <span className="item-order">Cant: </span>
-                        <span className="underline">{it.qty}</span>
-                        <span className="item-order"> Art: </span>
-                        <span className="underline">{it.name}</span>
-                      </p>
-                    </div>
-                  );
-                })}
-                <p>
-                  <span className="item-order">Notas para Chef:</span>
-                  <span className="underline">{element.orderDetail.notes}</span>
-                </p>
-                <Button
-                  name="Preparando"
-                  action={() => {
-                    this.changeToPreparing(element.orderID);
-                  }}
-                />
-              </div>
-            );
-          })}
+      <section className="kitchen-orders">
+        <div className="orders border-right">
+          <h3> Pedidos Nuevos </h3>
+          <div>
+            {this.state.newOrders.map((element, i) => {
+              return (
+                <div id={element.orderID} key={i} className="item-kitchen">
+                  <span className="underline">
+                    Mesa: {element.orderDetail.table}
+                  </span>
+                  {element.orderDetail.items.map((it, i) => {
+                    return (
+                      <div key={i}>
+                        <p>
+                          <span className="item-order">Cant: </span>
+                          <span className="underline">{it.qty}</span>
+                          <span className="item-order"> Art: </span>
+                          <span className="underline">{it.name}</span>
+                        </p>
+                      </div>
+                    );
+                  })}
+                  <p>
+                    <span className="item-order">Notas para Chef:</span>
+                    <span className="underline">
+                      {element.orderDetail.notes}
+                    </span>
+                  </p>
+                  <Button
+                    name="Preparando"
+                    action={() => {
+                      this.changeStatus(element.orderID, "prep");
+                      this.getNewOrders();
+                    }}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="orders">
+          <h3> En Preparación </h3>
+          <div>
+            {this.state.prepOrders.map((element, i) => {
+              return (
+                <div
+                  id={element.orderID}
+                  key={i}
+                  className="item-kitchen item-prep"
+                >
+                  <span className="underline">
+                    Mesa: {element.orderDetail.table}
+                  </span>
+                  {element.orderDetail.items.map((it, i) => {
+                    return (
+                      <div key={i}>
+                        <p>
+                          <span className="item-order">Cant: </span>
+                          <span className="underline">{it.qty}</span>
+                          <span className="item-order"> Art: </span>
+                          <span className="underline">{it.name}</span>
+                        </p>
+                      </div>
+                    );
+                  })}
+                  <p>
+                    <span className="item-order">Notas para Chef:</span>
+                    <span className="underline">
+                      {element.orderDetail.notes}
+                    </span>
+                  </p>
+                  <Button
+                    name="Listo"
+                    action={() => {
+                      this.changeStatus(element.orderID, "ready");
+                    }}
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </section>
     );
